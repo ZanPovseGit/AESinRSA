6aededd31292f47ecf7c215ce2b5c5ed9086c0a5
ZanPovseGit <159925134+ZanPovseGit@users.noreply.github.com>
2024-03-23 15:07:34 +0100

    Add files via upload

---
+++ AESinRSA/AESinRSA/AESklas.cs
@@ -0,0 +1,199 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Security.Cryptography;
+using System.IO;
+using Microsoft.Win32;
+
+namespace AESinRSA
+{
+    class AESklas
+    {
+        AesCryptoServiceProvider AEScript;
+
+        public AESklas()
+        {
+            AEScript = new AesCryptoServiceProvider();
+
+            AEScript.BlockSize = 128;
+            //AEScript.KeySize = 256;
+            //AEScript.GenerateIV();
+            AEScript.Padding = PaddingMode.PKCS7;
+            AEScript.Mode = CipherMode.CFB;
+        }
+        public void AES_Encrypt(string inputFile, string password,int keysize)
+        {
+
+            switch (keysize)
+            {
+                case 256:
+                    AEScript.KeySize = 256;
+                    
+                    break;
+                case 128:
+                    AEScript.KeySize = 128;
+                    
+                    break;
+                case 192:
+                    
+                    AEScript.KeySize = 192;
+                    break;
+                default:
+                    
+                    AEScript.KeySize = 256;
+                    break;
+            }
+
+            byte[] gesloSkriv = UstvariSol();
+
+            FileStream dat = new FileStream(inputFile + ".aes", FileMode.Create);
+
+            byte[] passwordBytes = System.Text.Encoding.UTF8.GetBytes(password);
+
+            var kljuc = new Rfc2898DeriveBytes(passwordBytes, gesloSkriv, 50000);
+            AEScript.Key = kljuc.GetBytes(AEScript.KeySize / 8);
+            AEScript.IV = kljuc.GetBytes(AEScript.BlockSize / 8);
+
+            dat.Write(gesloSkriv, 0, gesloSkriv.Length);
+
+            CryptoStream enkrip = new CryptoStream(dat, AEScript.CreateEncryptor(), CryptoStreamMode.Write);
+
+            FileStream vpisi = new FileStream(inputFile, FileMode.Open);
+
+            byte[] blok = new byte[1048576];
+            int st;
+
+            while ((st = vpisi.Read(blok, 0, blok.Length)) > 0)
+            {
+               enkrip.Write(blok, 0, st);
+            }
+
+            vpisi.Close();
+
+            enkrip.Close();
+            dat.Close();
+            AEScript.Clear();
+
+        }
+
+        public void AES_Decrypt(string inputFile, string password, int keysize, out string mm)
+        {
+            mm = "";
+ 
+            switch (keysize)
+            {
+                case 256:
+                    AEScript.KeySize = 256;
+                    break;
+                case 128:
+                    AEScript.KeySize = 128;
+                    break;
+                case 192:
+                    AEScript.KeySize = 192;
+                    break;
+                default:
+                    AEScript.KeySize = 256;
+                    break;
+            }
+
+            byte[] kodaKluc = System.Text.Encoding.UTF8.GetBytes(password);
+            byte[] gesloSkriv = new byte[32];
+
+            FileStream datNot = new FileStream(inputFile, FileMode.Open);
+            datNot.Read(gesloSkriv, 0, gesloSkriv.Length);
+
+            var key = new Rfc2898DeriveBytes(kodaKluc, gesloSkriv, 50000);
+            AEScript.Key = key.GetBytes(AEScript.KeySize / 8);
+            AEScript.IV = key.GetBytes(AEScript.BlockSize / 8);
+
+            CryptoStream kodirni = new CryptoStream(datNot, AEScript.CreateDecryptor(), CryptoStreamMode.Read);
+
+            FileStream shrani = new FileStream(inputFile + ".dekriptirano", FileMode.Create);
+
+            int st;
+            byte[] bloki = new byte[1048576];
+
+            try
+            {
+                while ((st = kodirni.Read(bloki, 0, bloki.Length)) > 0)
+                {
+                    shrani.Write(bloki, 0, st);
+                }
+            }
+            catch (System.Security.Cryptography.CryptographicException ex_CryptographicException)
+            {
+                mm = "Napacen kljuc";
+            }
+
+
+            try
+            {
+                kodirni.Close();
+            }
+            catch (Exception ex)
+            {
+            }
+            finally
+            {
+                shrani.Close();
+                datNot.Close();
+                AEScript.Clear();
+
+            }
+        }
+
+        public byte[] UstvariSol()
+        {
+
+            byte[] sol = new byte[32];
+
+            using (RNGCryptoServiceProvider rng = new RNGCryptoServiceProvider())
+                for (int i = 0; i < 10; i++)
+                    rng.GetBytes(sol);
+
+            return sol;
+        }
+        public void RSAEncrypt(string klucEnkrip)
+        {
+            var RSAENK = new RSACryptoServiceProvider();
+
+            string publPrivKluc = RSAENK.ToXmlString(true);  
+            byte[] dataZaEnk = Encoding.ASCII.GetBytes(klucEnkrip);
+            byte[] EnkrRsa = RSAENK.Encrypt(dataZaEnk, false);
+
+            SaveFileDialog fil = new SaveFileDialog();
+            fil.Title = "Shranjevanje kljuca v Xml";
+            if (fil.ShowDialog() == true)
+            {
+                File.WriteAllText(fil.FileName + ".xml",publPrivKluc);
+            }
+            fil.Title = "Shranjevanje eknriptirane vrednosti";
+            if (fil.ShowDialog() == true)
+            {
+                File.WriteAllBytes(fil.FileName + ".txt", EnkrRsa);
+            }
+
+        }
+        public string RSADecript(string klu,string en)
+        {
+            string kluc=klu,enk=en;
+            
+            var RSAENK = new RSACryptoServiceProvider();
+
+            RSAENK.FromXmlString(kluc);
+
+            byte[] ekrip = File.ReadAllBytes(enk);
+
+            byte[] DekriptRsa = RSAENK.Decrypt(ekrip, false);
+
+            string rezult = Encoding.Default.GetString(DekriptRsa);
+
+            return rezult;
+        }
+
+
+
+    }
+}
---
+++ AESinRSA/AESinRSA/MainWindow.xaml.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+//using System.Windows.Shapes;
+using Microsoft.Win32;
+using System.IO;
+
+namespace AESinRSA
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog dialog = new OpenFileDialog();
+
+            dialog.Multiselect = false;
+
+            if (dialog.ShowDialog() == true)
+            {
+                pot.Text = dialog.FileName;
+            }
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            AESklas mm = new AESklas();
+            int a = 0; string izpis;
+            if (int.TryParse(vel.Text, out a) == true)
+            {
+                if (aese.IsChecked == true)
+                {
+                    mm.AES_Encrypt(pot.Text, geslo.Text, int.Parse(vel.Text));
+                }
+                if (aesd.IsChecked == true)
+                {
+                    mm.AES_Decrypt(pot.Text, geslo.Text, int.Parse(vel.Text),out izpis);
+                    if (izpis.Length == 0)
+                    {
+                        MessageBox.Show("Uspesno dekriptiranje");
+                    }
+                    else
+                    {
+                        MessageBox.Show(izpis);
+                    }
+                }
+            }
+            else
+            {
+                MessageBox.Show("Nepravilen vnos velikosti kjuca");
+            }
+        }
+
+        private void Button_Click_2(object sender, RoutedEventArgs e)
+        {
+            AESklas mm = new AESklas();
+            rsaen.Content = geslo.Text;
+            if(rsaen.Content!=null)
+            mm.RSAEncrypt(rsaen.Content.ToString());
+        }
+
+        private void Button_Click_3(object sender, RoutedEventArgs e)
+        {
+            AESklas mm = new AESklas();
+            string ac = "";
+            string kluc = "", enk = "";
+            OpenFileDialog odpri = new OpenFileDialog();
+            odpri.Title = "Kljuc za odkodiranje XML";
+            odpri.Filter = "XML Files|*.xml";
+            if (odpri.ShowDialog() == true)
+            {
+                
+                kluc = odpri.FileName;
+            }
+            odpri.Title = "Kriptiran kljuc";
+            odpri.Filter = "Text Files|*.txt";
+            if (odpri.ShowDialog() == true)
+            {
+                
+                enk = odpri.FileName;
+            }
+            kluc = File.ReadAllText(kluc);
+
+             ac = mm.RSADecript(kluc, enk);
+
+            dekripRSA.Content = ac;
+        }
+    }
+}
